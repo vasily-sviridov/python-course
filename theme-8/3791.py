b588fced8a48105eb193516fc69b1ff03a78548b
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-06-06 13:51:33 +0300

    Added solution for some tasks.

--- theme-8/3791.py
+++ theme-8/3791.py
@@ -4,10 +4,9 @@ import math
 def distance(x1: float, y1: float, x2: float, y2: float) -> float:
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
+
 x1 = float(input())
 y1 = float(input())
-
 x2 = float(input())
 y2 = float(input())
-
 print(distance(x1, y1, x2, y2))
