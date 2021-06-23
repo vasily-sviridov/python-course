e29a073e0187dc5f3ef05bdbe8b6d41da14cb77c
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-06-23 23:40:06 +0300

    Added solved task.

---
+++ ege/test/task_10.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+with open("24-5.txt") as f:
+    data = f.readline()
+
+    count = 0
+    for i in range(len(data) - 1):
+        if data[i] == "(" and data[i + 1] == ")":
+            count += 1
+            i += 2
+        if count == 10000:
+            print(i - 1)
+            break
