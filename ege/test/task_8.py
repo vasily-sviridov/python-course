030a0adc1b30e95d9335ab0bb1213dccda335b77
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-06-24 13:30:22 +0300

    Added more optimized solution of task_8.py.

--- ege/test/task_8.py
+++ ege/test/task_8.py
@@ -7,17 +7,9 @@
 Найдите количество таких чисел и максимальное из них. В ответе запишите сначала количество, а потом максимальное число.
 """
 
-def conv(n):
-    result = ""
-    while n:
-        result += str(n % 8)
-        n //= 8
-    
-    return result[::-1]
-
 buffer = []
 for i in range(99, 999):
-    if i % 10 == 9 and conv(i)[-1] == "1" and i % 18 != 0:
+    if i % 10 == 9 and oct(i)[-1] == "1" and i % 18 != 0:
         buffer.append(i)
 
 print(len(buffer), max(buffer))
